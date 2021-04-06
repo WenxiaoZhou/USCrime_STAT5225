8bdccf3d15a8a83953f5430fb9fee2b96f0afc69
WenxiaoZhou <wenxiao.zhou@uconn.edu>
2021-04-06 00:01:31 -0400

    Modified radar chart

--- Visualization_draft.R
+++ Visualization_draft.R
@@ -10,7 +10,6 @@ setwd("/Users/zhouwenxiao/Desktop/USCrime")
 load("fbi_2005to2019.RData")
 
 
-#Changes
 #(1)Bar plots in R Shiny
 #Top 5 ViolentCrime states within years and time zones 
 data_bar<-area_level_2005to2019[,c(1,2,3,4,5,6,11,15,16)]
@@ -41,11 +40,7 @@ data_bar$PropertyCrime1<-ifelse(data_bar$Population==0,0,round(data_bar$Property
 data_bar<-data_bar[-c(which(is.na(data_bar$ViolentCrime1)),which(is.na(data_bar$PropertyCrime1))),]
 data_bar_vio<-data_bar[with(data_bar,order(Year,-ViolentCrime1)),]
 data_bar_vio<-data_bar_vio[,c(1,2,3,5,11,12,13)]
-top_5_states_violent<-data_bar_vio %>%
-  filter(Section==input$Section)  %>%
-  filter(Year==input$Year)  %>%
-  filter(Area==input$Area) %>%
-  top_n(5,ViolentCrime1)
+
 library(shiny)
 ui<-fluidPage(
   titlePanel("Top 5 states in ViolentCrime!"),
@@ -68,10 +63,21 @@ ui<-fluidPage(
 )
 server<-function(input,output,session){
   output$plot_top_5_states_violent<-renderPlot({
+    top_5_states_violent<-data_bar_vio %>%
+      filter(Section==input$Section)  %>%
+      filter(Year==input$Year)  %>%
+      filter(Area==input$Area) %>%
+      top_n(5,ViolentCrime1)
     ggplot(top_5_states_violent,aes(x=State,y=ViolentCrime1))+
       geom_col(fill='#263e63')
   })
   output$table_top_5_states_details<-renderTable({
+    top_5_states_violent<-data_bar_vio %>%
+      filter(Section==input$Section)  %>%
+      filter(Year==input$Year)  %>%
+      filter(Area==input$Area) %>%
+      top_n(5,ViolentCrime1)
+    round(top_5_states_violent[,c(6,7)],digits=4)
     top_5_states_violent
   })
 }
@@ -82,6 +88,13 @@ shinyApp(ui=ui,server=server)
 
 
 
+
+
+
+#(2) Radar chart
+#change way to standardized, heatmap before radar chart to better intutive comparison
+#between states
+#https://www.r-graph-gallery.com/142-basic-radar-chart.html
 
 transform <- function(col, maximum = 100) {
   lambda_list <- MASS::boxcox(col ~ 1, 
@@ -97,8 +110,107 @@ transform <- function(col, maximum = 100) {
   (res - min) / (max - min) * maximum
 }
 
-data_comb<- state_rate_per_100k_2005to2019 %>% 
+data_rad_std<-state_rate_per_100k_2005to2019 %>% 
   mutate(across(4:14, transform))
+data_rad_std$State<-toupper(data_rad_std$State)
+
+#Choose 7 columns of crime types
+data_rad_std$Combinerape<-ifelse(is.na(data_rad_std$RapeLegacy),
+                                 data_rad_std$RapeRevised,
+                                 data_rad_std$RapeLegacy)
+data_rad1<-data_rad_std[,c(1,2,5,7,8,10,11,12,15)]
+
+#For some neighboring states, we explicitly want to compare their crime levels, 
+#so we choose to use the radar chart below
+
+library(ECharts2Shiny)
+library(shiny)
+CA_rad<-data_rad1[which(data_rad1$State=='CALIFORNIA'),]
+NE_rad<-data_rad1[which(data_rad1$State=='NEVADA'),]
+AR_rad<-data_rad1[which(data_rad1$State=='ARIZONA'),]
+CA_tr<-CA_rad %>%
+  filter(Year==2008) %>%
+  select(Murder,Robbery,AggravatedAssault,Burglary,LarcenyTheft,MotorTheft,Combinerape) %>%
+  t()
+NE_tr<-NE_rad %>%
+  filter(Year==2008) %>%
+  select(Murder,Robbery,AggravatedAssault,Burglary,LarcenyTheft,MotorTheft,Combinerape) %>%
+  t()
+AR_tr<-AR_rad %>%
+  filter(Year==2008) %>%
+  select(Murder,Robbery,AggravatedAssault,Burglary,LarcenyTheft,MotorTheft,Combinerape) %>%
+  t()
+#dat<-data.frame(CA=CA_tr,NE=NE_tr,AR=AR_tr)
+dat <- data.frame(CA = c(4300, 10000, 25000, 35000, 50000,25000,25000),
+                  NE = c(5000, 14000, 28000, 31000, 42000,31000,31000),
+                  AR = c(4000, 2000, 9000, 29000, 35000,35000,35000))
+#names(dat)<-c("CALIFORNIA","NEVADA","ARIZONA")
+row.names(dat)<-c("Murder","Robbery","AggrabatedAssault","Burglary","LarcenyTheft","MotorTheft","Rape")
+
+server <- function(input, output) {
+  renderRadarChart(div_id = "test",
+                   data = dat)
+}
+ui <- fluidPage(
+  loadEChartsLibrary(),
+  
+  tags$div(id="test", style="width:50%;height:400px;"),
+  deliverChart(div_id = "test")
+)
+
+shinyApp(ui = ui, server = server)
+
+
+
+#Comparison states that you choose within years
+library(shiny)
+library(fmsb)
+library(shinydashboard)
+
+ui<-fluidPage(
+  titlePanel("States Crime level Comparison"),
+  selectInput('State1','Select 1st state',selected='NEW YORK',
+              choices=unique(data_rad1$State)),
+  selectInput('State2','Select 2nd state',selected='MASSACHUSETTS',
+              choices=unique(data_rad1$State)),
+  selectInput('State3','Select 3rd state',selected='CONNECTICUT',
+              choices=unique(data_rad1$State)),
+  sliderInput('Year','Select year',value=2010,min=2005,max=2019),
+  plotOutput('plot_radar')
+)
+server<-function(input,output,session){
+  output$plot_radar<-renderPlot({
+    State1<-data_rad1 %>%
+      filter(State==input$State1) %>%
+      filter(Year==input$Year)
+    State2<-data_rad1 %>%
+      filter(State==input$State2) %>%
+      filter(Year==input$Year)
+    State3<-data_rad1 %>%
+      filter(State==input$State3) %>%
+      filter(Year==input$Year)
+    radar<-data.frame(rbind(State1,State2,State3))
+    radar<-radar[1:3,c(3:9)]
+    row.names(radar)<-c("State1","State2","State3")
+    radar<-rbind(rep(100,7),rep(0,7),radar)
+    colors_border = c(rgb(0.2, 0.5, 0.5, 0.9),
+                      rgb(0.8, 0.2, 0.5, 0.9) ,
+                      rgb(0.7, 0.5, 0.1, 0.9))
+    radarchart(radar,axistype=1,pcol=colors_border,
+               plwd = 4,plty = 1,cglcol = "grey",cglty = 1,
+               axislabcol = "grey",caxislabels = seq(0, 100, 10),
+               cglwd = 0.8,vlcex = 0.8)
+    legend(x = 1.5,y = 1,legend = rownames(radar[-c(1, 2),]),
+           bty = "n",
+           pch = 15 ,
+           col=colors_border,
+           text.col = "grey",
+           cex = 1.2,
+           pt.cex = 3)
+    
+  })
+}
+shinyApp(ui = ui, server = server)    
 
 
 
@@ -108,23 +220,22 @@ data_comb<- state_rate_per_100k_2005to2019 %>%
 
 
 
-
-
-
-#(2) Animated Bubble Chart
+#(3) Animated Bubble Chart
 #Method 1
 #More data need for better visualization: population, GDP, relatioship to Crime counts
 library(gapminder)
 library(ggplot2)
 library(gganimate)
-setwd("/Users/zhouwenxiao/Desktop/STAT5225-R and SAS Programming/Group Project")
 
-data_bubb<-data_comb
-pacific<-which(data_bubb$State %in% c("WASHINGTON","OREGON","CALIFORNIA","NEVADA","ALASKA","HAWAII"))
-mountain<-which(data_bubb$State %in% c("MONTANA","IDAHO","UTAH","ARIZONA","WYOMING","COLORADO","NEW MEXICO"))
-central<-which(data_bubb$State %in% c("NORTH DAKOTA","SOUTH DAKOTA","NEBRASKA","KANSAS","OKLAHOMA","TEXAS",
-                                     "MINNESOTA","IOWA","MISSOURI","ARKANSAS","LOUISIANA","WISCONSIN",
-                                     "ILLINOIS","TENNESSEE","MISSISSIPPI","ALABAMA"))
+data_bubb<-state_rate_per_100k_2005to2019[,c(1,2,3,4,9,13,14)]
+pacific<-which(data_bubb$State %in% capitalize(tolower(c("WASHINGTON","OREGON",
+                                                        "CALIFORNIA","NEVADA","ALASKA","HAWAII"))))
+mountain<-which(data_bubb$State %in% capitalize(tolower(c("MONTANA","IDAHO",
+                                                         "UTAH","ARIZONA","WYOMING","COLORADO","NEW MEXICO"))))
+central<-which(data_bubb$State %in% capitalize(tolower(c("NORTH DAKOTA","SOUTH DAKOTA",
+                                                        "NEBRASKA","KANSAS","OKLAHOMA","TEXAS",
+                                                        "MINNESOTA","IOWA","MISSOURI","ARKANSAS","LOUISIANA","WISCONSIN",
+                                                        "ILLINOIS","TENNESSEE","MISSISSIPPI","ALABAMA"))))
 index2<-c(pacific,mountain,central)
 index1<-seq(1,dim(data_bubb)[1],1)
 eastern<-index1[!index1 %in% index2]
@@ -132,6 +243,15 @@ data_bubb$index<-index1
 data_bubb$Section<-ifelse(data_bubb$index %in% pacific,"Pacific",
                          ifelse(data_bubb$index %in% mountain,"Mountain",
                                 ifelse(data_bubb$index %in% central,"Central","Eastern")))
+data_bubb$ViolentCrime1<-ifelse(data_bubb$Population==0,0,ifelse(is.na(data_bubb$ViolentCrimeRevised),
+                                                               round(data_bubb$ViolentCrimeLegacy/data_bubb$Population,4),
+                                                               round(data_bubb$ViolentCrimeRevised/data_bubb$Population,4)))
+
+data_bubb$PropertyCrime1<-ifelse(data_bubb$Population==0,0,round(data_bubb$PropertyCrime/data_bubb$Population,4))
+
+#Make a copy of the data to draw the bubble chart
+data_bubb1<-data_bubb
+
 data_bubb_rural<-data_bubb[which(data_bubb$Area=='Rural'),]
 data_bubb_rural$year<-as.integer(data_bubb_rural$year)
 ggplot(data_bubb_rural,aes(Population,ViolentCrime,size=PropertyCrime,color=Section))+
@@ -166,98 +286,6 @@ ggplot(data_bubb_rural,aes(Population,ViolentCrime,size = PropertyCrime,color=Se
 
 
 
-
-
-#(3) Radar chart
-#change way to standardized, heatmap before radar chart to better intutive comparison
-#between states
-#https://www.r-graph-gallery.com/142-basic-radar-chart.html
-data_rad_std<-data_comb
-data_rad_std$Murder<-data_rad_std$Murder/data_rad_std$Population*1000
-data_rad_std$Rape<-data_rad_std$Rape/data_rad_std$Population*1000
-data_rad_std$Robbery<-data_rad_std$Robbery/data_rad_std$Population*1000
-data_rad_std$AggravatedAssault<-data_rad_std$AggravatedAssault/data_rad_std$Population*1000
-data_rad_std$Burglary<-data_rad_std$Burglary/data_rad_std$Population*1000
-data_rad_std$LarcenyTheft<-data_rad_std$LarcenyTheft/data_rad_std$Population*1000
-data_rad_std$MotorTheft<-data_rad_std$MotorTheft/data_rad_std$Population*1000
-data_rad_std[,5:11]<-round(data_rad_std[,5:11],3)
-
-
-#Comparison between closed states
-library(shiny)
-library(ECharts2Shiny)
-CT_rad<-data_rad_std[which(data_rad_std$State=='CONNECTICUT'),]
-MA_rad<-data_rad_std[which(data_rad_std$State=='MASSACHUSETTS'),]
-NY_rad<-data_rad_std[which(data_rad_std$State=='NEW YORK'),]
-CT_rural<-t(CT_rad[which(CT_rad$Area=='Rural'&CT_rad$year==2000),5:11])
-MA_rural<-t(MA_rad[which(MA_rad$Area=='Rural'&MA_rad$year==2000),5:11])
-NY_rural<-t(NY_rad[which(NY_rad$Area=='Rural'&NY_rad$year==2000),5:11])
-dat_rad_com<-data.frame(CT=CT_rural,MA=MA_rural,NY=NY_rural)
-names(dat_rad_com)<-c("CONNECTICUT",'MASSACHUSETTS','NEW YORK')
-ui <- fluidPage(
-  # We MUST load the ECharts javascript library in advance
-  loadEChartsLibrary(),
-  tags$div(id="test", style="width:50%;height:400px;"),
-  deliverChart(div_id = "test")
-)
-server<-function(input, output) {
-  renderRadarChart(div_id = "test",
-                   data = dat_rad_com)
-}
-shinyApp(ui = ui, server = server)
-
-
-
-
-#Comparison states that you choose, within years, area
-data_rad1<-data_rad_std[which(data_rad_std$Area=='Metropolitan Statistical Area'),]
-library(shiny)
-library(fmsb)
-library(shinydashboard)
-ui<-fluidPage(
-  titlePanel("States Crime level Comparison"),
-  selectInput('State1','Select 1st state',selected='NEW YORK',
-              choices=unique(data_rad1$State)),
-  selectInput('State2','Select 2nd state',selected='MASSACHUSETTS',
-              choices=unique(data_rad1$State)),
-  selectInput('State3','Select 3rd state',selected='CONNECTICUT',
-              choices=unique(data_rad1$State)),
-  sliderInput('year','Select year',value=1999,min=1999,max=2004),
-  plotOutput('plot_radar')
-)
-server<-function(input,output,session){
-  output$plot_radar<-renderPlot({
-    State1<-data_rad1 %>%
-              filter(State==input$State1) %>%
-              filter(year==input$year)
-    State2<-data_rad1 %>%
-      filter(State==input$State2) %>%
-      filter(year==input$year)
-    State3<-data_rad1 %>%
-      filter(State==input$State3) %>%
-      filter(year==input$year) 
-    radar<-data.frame(rbind(State1,State2,State3))
-    radar<-radar[1:3,c(5:11)]
-    row.names(radar)<-c("State1","State2","State3")
-    radar<-rbind(rep(50,7),rep(0,7),radar)
-    colors_border = c(rgb(0.2, 0.5, 0.5, 0.9),
-                      rgb(0.8, 0.2, 0.5, 0.9) ,
-                      rgb(0.7, 0.5, 0.1, 0.9))
-      radarchart(radar,axistype=1,pcol=colors_border,
-                 plwd = 4,plty = 1,cglcol = "grey",cglty = 1,
-                 axislabcol = "grey",caxislabels = seq(0, 50, 10),
-                 cglwd = 0.8,vlcex = 0.8)
-      legend(x = 0.7,y = 1,legend = rownames(radar[-c(1, 2),]),
-        bty = "n",
-        pch = 1 ,
-        col = colors_in ,
-        text.col = "grey",
-        cex = 1.2,
-        pt.cex = 3)
-      
-  })
-}
-shinyApp(ui = ui, server = server)    
 
 
 
